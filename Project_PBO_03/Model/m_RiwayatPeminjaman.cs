c3012c80068b4ca82e1474a7793e47c8fc1d318e
rizalprihadi25@gmail.com <rizalprihadi94@gmail.com>
2024-06-01 21:06:37 +0700

    Add m_RiwayatPeminjaman

---
+++ Project_PBO_03/Model/m_RiwayatPeminjaman.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PBO_Main.App.Model
+{
+    internal class m_RiwayatPeminjaman
+    {
+        public int id_pembayaran { get; set; }
+        public string status_pengembalian { get; set; }
+        public int denda { get; set; }
+
+    }
+}
