7a833b81449500236766c38370a3ebddd8e8353f
Febyputr <Ofififi255@gmail.com>
2024-06-02 00:29:55 +0700

    V_admin,Belum fix

--- Project_PBO_03/Program.cs
+++ Project_PBO_03/Program.cs
@@ -11,7 +11,7 @@ namespace Project_PBO_03
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new v_loginDaftar());
+            Application.Run(new V_Admin());
         }
     }
 }
