da85996ca84c0891984b7c10786f2277e377b786
rizalprihadi25@gmail.com <rizalprihadi94@gmail.com>
2024-06-02 00:45:52 +0700

    Add PenggunaContex

--- Project_PBO_03/Model/m_UlasanBuku.cs
+++ Project_PBO_03/Model/m_UlasanBuku.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +8,6 @@ namespace Project_PBO_03.Model
 {
     internal class m_UlasanBuku
     {
-        [Key]
-        public int isbn_buku { get; set; }
-        [Key]
-        public int id_user { get; set; }
-        public string ulasan { get; set; }
+
     }
 }
