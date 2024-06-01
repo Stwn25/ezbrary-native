6725f0c516f9aaf204ac39469f1316b485036300
Stwn25 <msetiawanwibisono@gmail.com>
2024-06-01 21:00:11 +0700

    add m_JenisBuku

---
+++ Project_PBO_03/Model/m_JenisBuku.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PBO_03.Model
+{
+    internal class m_JenisBuku
+    {
+        [Key]
+        public int id_jenis { get; set; }
+        [Required]
+        public string nama_jenis { get; set; }
+    }
+}
