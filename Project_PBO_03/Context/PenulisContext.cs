b8aad46b4c96ad14f62c836e0edb431fecc82426
velinaaas <vlnary221@gmail.com>
2024-06-04 22:59:49 +0700

    add Context Penulis

---
+++ Project_PBO_03/Context/PenulisContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collection.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using Project_PBO_03.Model;
+using Project_PBO_03.Core;
+
+namespace Project_PBO_03.Context
+{
+    internal class PenulisContext: DBconnection
+    {
+        private string table : "penulis"
+
+        public static DataTable all()
+        {
+            string query = $"SELECT * FROM {table}"
+            Datatable dataPenulis = queryExecutor(query);
+            return dataPenulis;
+        }
+
+        public static void create(m_Penulis penulisBaru)
+        {
+            string query = $"INSERT INTO {table} (idpenulis, namapenulis) VALUES(@idpenulis, @namapenulis)";
+            NpgsqlParameter[] parameters =
+            {
+                new NpgsqlParameter("@idpenulis", NpgsqlDbType.Integer){Value = penulisBaru.id_penulis},
+                new NpgsqlParameter("@namapenulis", NpgsqlDbType.Varchar){Value = penulisBaru.nama_penulis},
+            };
+            commandExecutor(query, parameters);
+        }
+
+        public static void delete(int id) 
+        {
+            string query = $"DELETE FROM {table} WHERE idpenulis = @idpenulis";
+            NpgsqlParameter[] parameters =
+            {
+                new NpgsqlParameter("@idpenulis", NpgsqlDbType.Integer){Value = id},
+            };
+            commandExecutor(query, parameters);
+        }
+
+    }
+}
